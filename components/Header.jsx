cc370052d1dbb684d1fc8167258edcc896a1137d
sauldesigns <splat002@ucr.edu>
2021-04-09 01:02:45 -0700

    changes

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,27 +4,25 @@ import Avatar from './Avatar';
 
 export default function Header() {
 	return (
-		<header className="flex w-full">
-			<nav className='flex w-full p-5 justify-between text-sm text-gray-700'>
-				{/* Left */}
-				<div className='flex space-x-4 items-center'>
-					<p className='link'>About</p>
-					<p className='link'>Store</p>
-				</div>
-				{/* Right */}
-				<div className='flex space-x-4 items-center'>
-					<p className='link'>Gmail</p>
-					<p className='link'>Store</p>
-					{/* Icon */}
+		<header className='flex w-full p-5 justify-between text-sm text-gray-700'>
+			{/* Left */}
+			<div className='flex space-x-4 items-center'>
+				<p className='link'>About</p>
+				<p className='link'>Store</p>
+			</div>
+			{/* Right */}
+			<div className='flex space-x-4 items-center'>
+				<p className='link'>Gmail</p>
+				<p className='link'>Store</p>
+				{/* Icon */}
 
-					<ViewGridIcon className='h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer' />
-					<MicrophoneIcon className='h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer' />
+				<ViewGridIcon className='h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer' />
+				<MicrophoneIcon className='h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer' />
 
-					{/* Avatar */}
+				{/* Avatar */}
 
-					<Avatar url='https://bit.ly/3myTyzK' />
-				</div>
-			</nav>
+				<Avatar url='https://bit.ly/3myTyzK' />
+			</div>
 		</header>
 	);
 }
